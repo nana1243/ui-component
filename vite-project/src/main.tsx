5a1eb7186f78f4c43d78db0eac71c32dba8bdde7
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-11-05 07:47:05 +0900

    fea)[11/5] 라우팅 추가

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+import {routeTree} from './routeTree.gen';
+
+const router = createRouter({
+    routeTree,
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}>
+            <App/>
+        </RouterProvider>
+    </StrictMode>,
 )
