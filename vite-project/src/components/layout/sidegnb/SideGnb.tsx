bb40cefa4b4b87208137ed36b303201b03535040
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-11-08 11:30:12 +0900

    fea)[11/8] SidebarGNB GUI 적용

--- vite-project/src/components/layout/sidegnb/SideGnb.tsx
+++ vite-project/src/components/layout/sidegnb/SideGnb.tsx
@@ -1,14 +1,20 @@
 import { Link } from '@tanstack/react-router';
 import { gnbRootList } from '@/constants/routes';
+import styles from './index.module.css';
 
 function SideGnb() {
 
   return (
-    <aside>
-      <ul>
+    <aside class={styles.sidebar}>
+      <h1 class={styles.sidebarHeader}>
+        <Link href='/'>
+          UI 요소 모음 <sub>hennie</sub>
+        </Link>
+      </h1>
+      <ul class={styles.sidebarNav}>
         {gnbRootList.map((route) => (
-            <li key={route.key}>
-              <Link to={route.link}>{route.name}</Link>
+            <li key={route.key} class={styles.navItem}>
+              <Link to={route.link} class={styles.navLink}>{route.name}</Link>
             </li>
           ),
         )}
