e6571219433a71993b2aee38085f716870d56cf2
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-11-09 10:32:59 +0900

    fea)[11/9] SideGnb 웹접근성 고려

--- vite-project/src/components/layout/sidegnb/SideGnb.tsx
+++ vite-project/src/components/layout/sidegnb/SideGnb.tsx
@@ -5,12 +5,12 @@ import styles from './index.module.css';
 function SideGnb() {
 
   return (
-    <aside className={styles.sidebar}>
-      <h1 className={styles.sidebarHeader}>
-        <Link href='/'>
+    <aside className={styles.sidebar} role={'navigation'} aria-label='주요 UI 요소 메뉴'>
+      <h2 className={styles.sidebarHeader}>
+        <Link href='/' aria-label='홈으로 이동, UI 요소 모음'>
           UI 요소 모음 <sub>hennie</sub>
         </Link>
-      </h1>
+      </h2>
       <ul className={styles.sidebarNav}>
         {gnbRootList.map((route) => (
             <li key={route.key} className={styles.navItem}>
